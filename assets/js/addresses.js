1f414b9bf3afed40ab96ee92e5e62b39d4331b13
Cuidandoelagua517 <dgil@bairesanalitica.com>
2025-03-18 17:28:42 -0300

    Update addresses.js

--- assets/js/addresses.js
+++ assets/js/addresses.js
@@ -9,68 +9,126 @@
             this.initSetDefaultAddress();
         },
         
-        initDeleteAddress: function() {
-    $('.mam-delete-address').on('click', function(e) {
-        e.preventDefault();
-        
-        if (confirm(mam_params.i18n.confirm_delete)) {
-            var addressId = $(this).data('address-id');
-            var $item = $(this).closest('.mam-address-item');
+        initAddressForm: function() {
+            var self = this;
             
-            $.ajax({
-                type: 'POST',
-                url: mam_params.ajax_url,
-                data: {
-                    action: 'mam_delete_address',
-                    security: mam_params.nonce,
-                    address_id: addressId
-                },
-                success: function(response) {
-                    if (response.success) {
-                        $item.fadeOut(300, function() {
-                            $(this).remove();
-                        });
-                        MAM_Addresses.showMessage('success', response.data.message);
-                    } else {
-                        MAM_Addresses.showMessage('error', response.data.message);
+            // Manejar envío del formulario
+            $('.mam-address-form').on('submit', function(e) {
+                e.preventDefault();
+                
+                var $form = $(this);
+                var formData = $form.serialize();
+                
+                // Añadir nonce y acción
+                formData += '&action=mam_save_address&security=' + mam_params.nonce;
+                
+                // Deshabilitar botón y mostrar loader
+                var $submitBtn = $form.find('button[type="submit"]');
+                $submitBtn.prop('disabled', true).addClass('mam-loading');
+                
+                $.ajax({
+                    type: 'POST',
+                    url: mam_params.ajax_url,
+                    data: formData,
+                    success: function(response) {
+                        if (response.success) {
+                            // Actualizar lista de direcciones
+                            $('.mam-addresses-list').html(response.data.html);
+                            
+                            // Mostrar mensaje de éxito
+                            self.showMessage('success', response.data.message);
+                            
+                            // Cerrar formulario
+                            $('.mam-address-form-container').slideUp();
+                        } else {
+                            // Mostrar error
+                            self.showMessage('error', response.data.message);
+                        }
+                        
+                        // Restaurar botón
+                        $submitBtn.prop('disabled', false).removeClass('mam-loading');
+                    },
+                    error: function() {
+                        self.showMessage('error', 'Error de conexión. Por favor, inténtalo de nuevo.');
+                        $submitBtn.prop('disabled', false).removeClass('mam-loading');
                     }
-                },
-                error: function() {
-                    MAM_Addresses.showMessage('error', mam_params.i18n.error);
+                });
+            });
+            
+            // Mostrar formulario al hacer clic en "Añadir dirección"
+            $('.mam-add-address a').on('click', function(e) {
+                e.preventDefault();
+                $('.mam-address-form-container').slideDown();
+                $('html, body').animate({
+                    scrollTop: $('.mam-address-form-container').offset().top - 50
+                }, 500);
+            });
+        },
+        
+        initDeleteAddress: function() {
+            $('.mam-delete-address').on('click', function(e) {
+                e.preventDefault();
+                
+                if (confirm(mam_params.i18n.confirm_delete)) {
+                    var addressId = $(this).data('address-id');
+                    var $item = $(this).closest('.mam-address-item');
+                    
+                    $.ajax({
+                        type: 'POST',
+                        url: mam_params.ajax_url,
+                        data: {
+                            action: 'mam_delete_address',
+                            security: mam_params.nonce,
+                            address_id: addressId
+                        },
+                        success: function(response) {
+                            if (response.success) {
+                                $item.fadeOut(300, function() {
+                                    $(this).remove();
+                                });
+                                MAM_Addresses.showMessage('success', response.data.message);
+                            } else {
+                                MAM_Addresses.showMessage('error', response.data.message);
+                            }
+                        },
+                        error: function() {
+                            MAM_Addresses.showMessage('error', mam_params.i18n.error);
+                        }
+                    });
                 }
             });
-        }
-    });
-},
-  initSetDefaultAddress: function() {
-    $('.mam-set-default').on('click', function(e) {
-        e.preventDefault();
+        },
         
-        var addressId = $(this).data('address-id');
-        var addressType = $(this).data('address-type');
+        initSetDefaultAddress: function() {
+            $('.mam-set-default').on('click', function(e) {
+                e.preventDefault();
+                
+                var addressId = $(this).data('address-id');
+                var addressType = $(this).data('address-type');
+                
+                $.ajax({
+                    type: 'POST',
+                    url: mam_params.ajax_url,
+                    data: {
+                        action: 'mam_set_default_address',
+                        security: mam_params.nonce,
+                        address_id: addressId,
+                        address_type: addressType
+                    },
+                    success: function(response) {
+                        if (response.success) {
+                            window.location.reload();
+                        } else {
+                            MAM_Addresses.showMessage('error', response.data.message);
+                        }
+                    },
+                    error: function() {
+                        MAM_Addresses.showMessage('error', mam_params.i18n.error);
+                    }
+                });
+            });
+        },
         
-        $.ajax({
-            type: 'POST',
-            url: mam_params.ajax_url,
-            data: {
-                action: 'mam_set_default_address',
-                security: mam_params.nonce,
-                address_id: addressId,
-                address_type: addressType
-            },
-            success: function(response) {
-                if (response.success) {
-                    window.location.reload();
-                } else {
-                    MAM_Addresses.showMessage('error', response.data.message);
-                }
-            },
-            error: function() {
-                MAM_Addresses.showMessage('error', mam_params.i18n.error);
-            }
-        });
-    });
-}      
         showMessage: function(type, message) {
             var $messageContainer = $('.mam-messages');
             
@@ -88,9 +146,7 @@
                     $(this).remove();
                 });
             }, 5000);
-        },
-        
-        // Implementar initDeleteAddress y initSetDefaultAddress
+        }
     };
     
     $(document).ready(function() {
