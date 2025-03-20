9e01b033d0e2945f3e5ef017ac75a61dc604dbc2
Cuidandoelagua517 <dgil@bairesanalitica.com>
2025-03-20 10:15:43 -0300

    Update my-account-manager.php

--- my-account-manager.php
+++ my-account-manager.php
@@ -209,7 +209,7 @@ public function register_ajax_handlers() {
     // Registrar handlers globales para la aplicación
     add_action('wp_ajax_nopriv_mam_ajax_login', array($this, 'handle_ajax_login'));
     add_action('wp_ajax_nopriv_mam_ajax_register', array($this, 'handle_ajax_register'));
-action('wp_ajax_mam_user_action', array($this, 'handle_user_action'));
+add_action('wp_ajax_mam_user_action', array($this, 'handle_user_action'));
 }
 /**
  * Manejar solicitud AJAX de login (puente al módulo)
