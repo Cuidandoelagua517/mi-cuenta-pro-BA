fd8e9b9ba38c0fe2043fc43da270c8d200981ffb
Cuidandoelagua517 <dgil@bairesanalitica.com>
2025-03-24 00:58:13 -0300

    Update class-mam-addresses.php

--- includes/modules/class-mam-addresses.php
+++ includes/modules/class-mam-addresses.php
@@ -26,7 +26,7 @@ class MAM_Addresses {
      */
     public function __construct() {
         // Añadir campos a la edición de cuenta
-    add_action('woocommerce_edit_account_form', array($this, 'load_account_fields_values'));
+    //add_action('woocommerce_edit_account_form', array($this, 'load_account_fields_values'));
     add_action('woocommerce_save_account_details', array($this, 'save_account_fields'));
     add_filter('woocommerce_form_field_args', array($this, 'add_form_field_args'), 10, 3);
         // Personalizar título del endpoint
