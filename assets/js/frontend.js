b438f3355465ee222be7e04499557de085d79f1b
Cuidandoelagua517 <dgil@bairesanalitica.com>
2025-04-09 17:43:01 -0300

    Update frontend.js

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -95,7 +95,40 @@
                 });
             });
         },
+        /**
+ * Funcionalidades específicas para formularios en popups
+ */
+function initPopupForms() {
+    // Toggle entre formularios de login/registro dentro de un popup
+    $('.mam-toggle-form').on('click', function(e) {
+        e.preventDefault();
         
+        var targetForm = $(this).data('toggle');
+        
+        if (targetForm === 'register') {
+            $('.mam-login-popup-form').hide();
+            $('.mam-register-popup-form').show();
+        } else {
+            $('.mam-register-popup-form').hide();
+            $('.mam-login-popup-form').show();
+        }
+    });
+    
+    // Mostrar/ocultar contraseña en popups
+    $('.mam-popup-form .mam-password-toggle').on('click', function() {
+        var $passwordField = $(this).closest('.mam-password-field').find('input');
+        
+        if ($passwordField.attr('type') === 'password') {
+            $passwordField.attr('type', 'text');
+            $(this).addClass('showing-password');
+            $(this).find('.mam-eye-icon').html('<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />');
+        } else {
+            $passwordField.attr('type', 'password');
+            $(this).removeClass('showing-password');
+            $(this).find('.mam-eye-icon').html('<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />');
+        }
+    });
+}
         /**
          * Inicializar registro por AJAX
          */
@@ -519,7 +552,8 @@ $(document).ready(function() {
         }
     });
 }
-    
+        // Inicializar funciones para popups
+    initPopupForms();
     // Validación al perder el foco
     $('#reg_cuit').on('blur', function() {
         var $field = $(this);
